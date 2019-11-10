e98675f4a1e1528817e9743972e0446aee9b62d3
Andres <a2payema@gmail.com>
2019-11-10 02:40:22 -0400

    progress in show menu and get menu functions. Still problems with assign values

--- main.c
+++ main.c
@@ -60,6 +60,9 @@ struct pedidos{
 bool check_user(struct users *current_user, int *logout);
 void cadastrar_pedido(struct users *atendente,  struct clientes *cliente);
 int check_cliente(char cpf[], struct clientes *cliente);
+void show_menu();
+void get_menu(struct items (*menu)[Max]);
+
 int main ()
 {
     //testuser
@@ -104,6 +107,7 @@ int main ()
 			case 2:
 			break;
 			case 3:
+			    show_menu();
 			break;
 			case 4:
 			break;
@@ -140,7 +144,6 @@ void cadastrar_pedido(struct users *atendente, struct clientes *pcliente){
     scanf("%s", cpf);
     if(check_cliente(cpf, pcliente)){
         ppedido->cliente= *pcliente;
-        printf("%s", ppedido->cliente.CPF);
     }
 
 }
@@ -149,12 +152,12 @@ void cadastrar_pedido(struct users *atendente, struct clientes *pcliente){
 int check_cliente(char cpf[], struct clientes *pcliente){
     int i=0;
     int length;
+    //for testing purpose
     struct clientes test_cliente[2]= {
     {1,"789456","andres","Manaus","987456123"},
     {1,"123456","simon","Peru","987456111"}
     };
     length= sizeof(test_cliente)/sizeof(test_cliente[0]);
-//not working
     for(;i<length-1;i++ ){
             if(strcmp(cpf,test_cliente[i].CPF)==0){
                 printf("cliente no sistema!\n");
@@ -164,4 +167,45 @@ int check_cliente(char cpf[], struct clientes *pcliente){
     }
     printf("cliente sem registro\n");
  return 0;
+}
+
+void show_menu(){
+    system("@cls||clear");
+    int opt,i, length;
+    struct items (*pmenu)[Max], menu[Max];
+    pmenu= &menu;
+    get_menu(pmenu);
+    length= sizeof(menu)/sizeof(menu[0]);
+    printf("%s", menu[1].nome);
+    printf("---------Menu------\n");
+    for(i=0;i< length-1; i++){
+        printf("%d- %s ->>>>>> %f\n",i, menu[i].nome, menu[i].prize);
+
+    }
+    scanf("%d", &opt);
+
+}
+
+void get_menu(struct items (*menu)[Max]){
+    //for testing purpose
+    int i, lenght;
+    struct produtos prod1={1,"item A", 5, 10};
+    struct items test_user[2]={{.id=1,"pizza A",.prize= 22.5,{prod1},.quantidade=5},{.id= 2,"pizza B",.prize= 22.8,{prod1},.quantidade= 5}};
+
+    lenght= sizeof(test_user)/sizeof(test_user[0]);
+    for(i=0;i<=lenght-1;i++){
+        //assign items to menu
+        menu[i]->id= test_user[i].id;
+        strcpy(menu[i]->nome,test_user[i].nome);
+        (*menu[i]).prize= test_user[i].prize;
+        (*menu[i]).produto->id = prod1.id;
+        strcpy((*menu[i]).produto->nome, prod1.nome);
+        (*menu[i]).produto->prize= prod1.prize;
+        (*menu[i]).produto->quantidade = prod1.quantidade;
+        (*menu[i]).quantidade= test_user[i].quantidade;
+        //testing values assings
+        //printf("Items del menu:\n id: %d \nitem name:%s\nprize: %.2f \n Quantidade: %d\n ",menu[i]->id, menu[i]->nome, (*menu[i]).prize, (*menu[i]).quantidade  );
+        //printf("produtos usados:\n id: %d \n item name:%s\nprize: %.2f \n Quantidade: %d\n",(*menu[i]).produto->id, (*menu[i]).produto->nome,(*menu[i]).produto->prize,  (*menu[i]).produto->quantidade  );
+    }
+    printf("values assign !");
 }
